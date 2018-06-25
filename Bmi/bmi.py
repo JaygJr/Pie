85e712efba6399472d5a66580d323297f61f0d62
Jay Gherlone <sixgun.sigma@gmail.com>
2018-06-25 13:56:58 -0400

    Clean code via Pylint

--- Bmi/bmi.py
+++ Bmi/bmi.py
@@ -1,71 +1,67 @@
-from gpiozero import LED
 from time import sleep
-
-# bred = 17 yellow =27 green =22 
-red_led      = LED(17) # under weight and obese
-yellow_led   = LED(27) # overweight
-green_led    = LED(22) # normal weight
-
+from gpiozero import LED
+# bred = 17 yellow =27 green =22
+RED_LED      = LED(17) # under weight and obese
+YELLOW_LED   = LED(27) # overweight
+GREEN_LED    = LED(22) # normal weight
 # Welcome to JAYY BMI Calculator calculator
-print ("Welcome to JAYS BMI Calculator")
-print ()
+print("Welcome to JAYS BMI Calculator")
+print()
 
-# Get height from user, 
+# Get height from user,
 # Code for get  inches initially.Then later, change to ft/in.
 # Then convert back to inches
 # Get height from user
-height = int(input("Please enter your height in inches: "))
-##print (height)
+HEIGHT = int(input("Please enter your height in inches: "))
+##print(height)
 
 # Convert from inches to meters
 # inches * 0.0254
-meters = height * .0254
-print ("your height in meters is: " + str(meters))
+METERS = HEIGHT * .0254
+print("your height in meters is: " + str(METERS))
 
 # Convert from meters to centimeters
 # multiply meters X 100
-centimeters = meters * 100
-print ("your height in centimeters is: " + str(centimeters))
+CENTIMETERS = METERS * 100
+print("your height in centimeters is: " + str(CENTIMETERS))
 
 
 # Get weight from user
-weight = int(input("Please enter your weight in pounds: "))
-##print (weight)
-print ()
+WEIGHT = int(input("Please enter your weight in pounds: "))
+##print (WEIGHT)
+print()
 
 #convert from pounds to kilograms 
 #kg = lb / 2.2046
-kilograms = weight / 2.2046
-print("your weight in kg is: " + str(kilograms))
+KILOGRAMS = WEIGHT / 2.2046
+print("your weight in kg is: " + str(KILOGRAMS))
 
 #compute BMI
 # BMI = kilograms / meters squared
 #Display answer to user
-bmi = kilograms / meters**2
-print ("Your BMI is: " + str(bmi))
+BMI = KILOGRAMS / METERS**2
+print("Your BMI is: " + str(BMI))
 
 # If BMI < 18.5  print 'underweight' 
-if bmi  < 18.5:
-    print ("underweight")
-    red_led.on()
+if BMI  <18.5:
+    print("underweight")
+    RED_LED.on()
     sleep(5)
 
 # if bmi >= 18.5 and < 25 print 'normal' 
-elif bmi >= 18.5 and  bmi < 25:
-    print ("normal")    
-    green_led.on()
+elif BMI >=18.5 and  BMI< 25:
+    print("normal")    
+    GREEN_LED.on()
     sleep(5)
 
 # If bmi >= 25 and  bmi < 30 print 'overweight'
-elif bmi >= 25 and  bmi < 30: 
-    print ("overweight")
-    yellow_led.on()
+elif BMI >=25 and  BMI <30: 
+    print("overweight")
+    YELLOW_LED.on()
     sleep(5)
 
 # If bmi >= 30 print 'obese'
-elif bmi >=  30:
-    print ("obese")
-    red_led.on()
+elif BMI >=30:
+    print("obese")
+    RED_LED.on()
     sleep(5)
-
-
